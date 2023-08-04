9cc57ec580f335a64f05d3722535fb9b2d446234
zaidancarvalho <felipecfernandez@hotmail.com>
2023-08-04 12:34:22 -0300

    Test 2 with database access

--- src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
+++ src/main/java/com/devsuperior/dscommerce/controllers/ProductController.java
@@ -1,15 +1,26 @@
 package com.devsuperior.dscommerce.controllers;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.devsuperior.dscommerce.entities.Product;
+import com.devsuperior.dscommerce.repositories.ProductRepository;
 
 @RestController
 @RequestMapping(value = "/products")
 public class ProductController {
 
+	@Autowired
+	private ProductRepository repository;
+	
 	@GetMapping
 	public String test() {
-		return "Ola mundo!";
+		Optional<Product> result = repository.findById(1L);
+		Product product = result.get();
+		return product.getName();
 	}
 }
