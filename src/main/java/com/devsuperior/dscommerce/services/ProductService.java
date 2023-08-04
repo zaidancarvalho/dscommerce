043b319ec558af625d7084623fd3cc35587989df
zaidancarvalho <felipecfernandez@hotmail.com>
2023-08-04 15:41:19 -0300

    GET Products paged

--- src/main/java/com/devsuperior/dscommerce/services/ProductService.java
+++ src/main/java/com/devsuperior/dscommerce/services/ProductService.java
@@ -1,8 +1,8 @@
 package com.devsuperior.dscommerce.services;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -20,5 +20,10 @@ public class ProductService {
 	public ProductDTO findById(Long id) {
 		Product product = repository.findById(id).get();
 		return new ProductDTO(product);
+	}
+	
+	public Page<ProductDTO> findAll(Pageable pageable) {
+		Page<Product> result = repository.findAll(pageable);
+		return result.map(x -> new ProductDTO(x));
 	}
 }
